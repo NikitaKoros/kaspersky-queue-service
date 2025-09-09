1eb7a7f7b7da1b6ff120d5178d25f08609354882
NikitaKoros <nikitakorostin09@gmail.com>
2025-09-09 20:56:30 +0300

    server and handlers

---
+++ internal/handler/handlers.go
@@ -0,0 +1,94 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/NikitaKoros/queue-serivce/internal/controller"
+)
+
+func HandleEnqueue(ctrl controller.ControllerProvider) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !ctrl.IsAccepting() {
+			writeErrorResponce(w, http.StatusServiceUnavailable, "Service is shutting down")
+			return
+		}
+
+		if r.Method != http.MethodPost {
+			writeErrorResponce(w, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+
+		var req controller.EnqueueRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			writeErrorResponce(w, http.StatusBadRequest, "Invalid JSON")
+			return
+		}
+
+		if req.ID == "" {
+			writeErrorResponce(w, http.StatusBadRequest, "ID is required")
+			return
+		}
+
+		if req.MaxRetries < 0 {
+			writeErrorResponce(w, http.StatusBadRequest, "max_retries must be >= 0")
+			return
+		}
+
+		taskID, err := ctrl.EnqueueTask(req)
+		if err != nil {
+			switch err {
+			case controller.ErrQueueFull:
+				writeErrorResponce(w, http.StatusServiceUnavailable, "Queue is full")
+				return
+			case controller.ErrTaskExists:
+				writeErrorResponce(w, http.StatusConflict, "Task with this ID already exists and is active")
+				return
+			default:
+				writeErrorResponce(w, http.StatusInternalServerError, "Failed to enqueue task")
+				return
+			}
+		}
+		
+		response := controller.EnqueueResponse {
+			Message: "Task enqueued successfully",
+			TaskID: taskID,
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func HandleHealth(ctrl controller.ControllerProvider) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			writeErrorResponce(w, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+		
+		taskStats, queueStats := ctrl.GetHealthStats()
+		
+		response := controller.HealthResponse{
+			Status: "ok",
+			Stats: taskStats,
+			Queue: queueStats,
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func writeErrorResponce(w http.ResponseWriter, statusCode int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	errorResponse := map[string]string{
+		"error": message,
+	}
+
+	json.NewEncoder(w).Encode(errorResponse)
+}
---
+++ internal/handler/server.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/NikitaKoros/queue-serivce/internal/controller"
+)
+
+type Server struct {
+	httpServer *http.Server
+	ctrl controller.ControllerProvider
+}
+
+func NewServer(addr string, ctrl controller.ControllerProvider) *Server {
+	mux := http.NewServeMux()
+	
+	mux.HandleFunc("/enqueue", HandleEnqueue(ctrl))
+	mux.HandleFunc("/healthz", HandleHealth(ctrl))
+	
+	httpServer := http.Server{
+		Addr: addr,
+		Handler: mux,
+	}
+	
+	return &Server{
+		httpServer: &httpServer,
+		ctrl: ctrl,
+	}
+}
+
+func (s *Server) Start() error {
+	log.Printf("Starting http server on %s", s.httpServer.Addr)
+	
+	err := s.httpServer.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) {
+	log.Println("Shutting down HTTP server...")
+	
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		log.Printf("HTTP server shutdown error: %v", err)
+	} else {
+		log.Println("HTTP server shut up successfuly")
+	}
+}
